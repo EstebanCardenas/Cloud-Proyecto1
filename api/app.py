6267c1bd549a43b6949778f0853687c6f1bb8ebb
EstebanCardenas <53832358+EstebanCardenas@users.noreply.github.com>
2021-02-07 19:57:18 -0500

    modelos

--- api/app.py
+++ api/app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, request, json
+from flask_cors.extension import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+#modelos
+from models import *
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+CORS(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-
 
 if __name__ == '__main__':
     app.run()
---
+++ api/models.py
@@ -0,0 +1,49 @@
+from app import db, ma
+
+#Usuario admin
+class UserAdmin(db.Model):
+    id = db.Column(db.Integer, primary_key=1)
+    nombres = db.Column(db.String(220), nullable=0)
+    apellidos = db.Column(db.String(220), nullable=0)
+    email = db.Column(db.String(120), nullable=0, unique=1)
+    contrasena = db.Column(db.String(120), nullable=0)
+    #relaciones
+    concursos = db.relationship('Concurso', backref='UserAdmin', lazy=1)
+class UserAdminSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "nombres", "apellidos", "email")
+userAdminSchema = UserAdminSchema()
+usersAdminSchema = UserAdminSchema(many=1)
+
+#Concurso
+class Concurso(db.Model):
+    id = db.Column(db.Integer, primary_key=1)
+    nombre = db.Column(db.String(120), nullable=0)
+    #TODO banner o imagen
+    #TODO url única
+    f_inicio = db.Column(db.DateTime, nullable=0)
+    f_fin = db.Column(db.DateTime, nullable=0)
+    valor_paga = db.Column(db.Integer, nullable=0)
+    guion = db.Column(db.String(2000), nullable=0)
+    recomendaciones = db.Column(db.String(2000), nullable=0)
+    #relaciones
+    user_id = db.Column(db.Integer, db.ForeignKey('UserAdmin.id'), nullable=0)
+    voces = db.relationship('Voz', backref='Concurso', lazy=1)
+class ConcursoSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "nombre", "f_inicio", "f_fin", "valor_paga", "guion", "recomendaciones", "user_id")
+concursoSchema = ConcursoSchema()
+concursosSchema = ConcursoSchema(many=1)
+
+class Voz(db.Model):
+    id = db.Column(db.Integer, primary_key=1)
+    f_creacion = db.Column(db.DateTime, nullable=0)
+    email = db.Column(db.String(120), unique=1, nullable=0)
+    nombres = db.Column(db.String(220), nullable=0)
+    apellidos = db.Column(db.String(220), nullable=0)
+    convertida = db.Column(db.Boolean, nullable=0) #estado de la voz
+    observaciones = db.Column(db.String(1000))
+    #TODO archivo_original
+    #TODO archivo_convertida
+    #relaciones
+    concurso_id = db.Column(db.Integer, db.ForeignKey('Concurso.id'), nullable=0)
