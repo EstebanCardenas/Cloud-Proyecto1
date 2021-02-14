49c853fa39ebf322192af18fe35e86e606bb72c0
SeCardenas <se.cardenas@uniandes.edu.co>
2021-02-14 12:25:25 -0800

    Modelos arreglados y actualizados, más endpoints

--- api/app.py
+++ api/app.py
@@ -1,8 +1,14 @@
+import random, string
+import os
 from flask import Flask, request, jsonify
 from flask_cors.extension import CORS
 from flask_praetorian import Praetorian, auth_required, current_user
+from werkzeug.utils import secure_filename
 #modelos
 from models import *
+
+UPLOAD_FOLDER = './originales/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top secret'
@@ -10,13 +16,31 @@ app.config['JWT_ACCESS_LIFESPAN'] = {'hours': 24}
 app.config['JWT_REFRESH_LIFESPAN'] = {'days': 30}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 CORS(app)
 guard = Praetorian()
 
+#with app.app_context():
+db.init_app(app)
+ma.init_app(app)
+guard.init_app(app, UserAdmin)
+
 with app.app_context():
-    db.init_app(app)
-    ma.init_app(app)
-    guard.init_app(app, UserAdmin)
+    db.create_all()
+
+
+def generate_url():
+    characters = string.ascii_letters + string.digits
+    url = ''.join(random.choices(characters, k=16))
+    while db.session.query(Concurso).filter_by(url=url).count() > 0:
+        url = ''.join(random.choices(characters, k=16))
+    return url
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
@@ -71,16 +95,26 @@ def concursos():
         valor_paga = req.get('valor_paga',None)
         guion = req.get('valor_paga',None)
         recomendaciones = req.get('recomendaciones',None)
+        imagen = req.get('imagen_base64',None)
+        url = req.get('url',None)
         #TODO URL
         if not nombre or not f_inicio or not f_fin or \
             not valor_paga or not guion or not recomendaciones:
             return jsonify({"msg": "Formulario incompleto"}), 400
+        if url:
+            if db.session.query(Concurso).filter_by(url=url).count() > 0:
+                return jsonify({"msg": "el url ya está usado"}), 400
+        else:
+            url = generate_url()
+
         concurso = Concurso(
             nombre=nombre,
             f_inicio=f_inicio,
             f_fin=f_fin,
             valor_paga=valor_paga,
             guion=guion,
+            imagen_base64=imagen,
+            url=url,
             recomendaciones=recomendaciones,
             user_id=user.id
             )
@@ -127,6 +161,61 @@ def concurso(concurso_id):
         db.session.commit()
         return '',204
 
+
+@app.route('/api/url/<string:concurso_url>', methods=['GET'])
+def concursoUrl(concurso_url):
+    concurso = db.session.query(Concurso).filter_by(url=url).first()
+    if not concurso:
+        return jsonify({"msg":"No existe ningun concurso con la url especificada"}),404
+    return concursoSchema.dump(concurso),200
+
+
+@app.route('/api/audio', methods=['POST'])
+def audio():
+    if 'file' not in request.files:
+        return jsonify({"msg":"El archivo de audio es requerido"}),400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"msg":"El archivo de audio es requerido"}),400
+    if allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        path = os.path.join(app.config['UPLOAD_FOLDER'],'id/', filename)
+        archivo_voz = ArchivoVoz(
+            archivo_original=filename,
+            convertido=False
+            )
+        path_real = os.path.join(app.config['UPLOAD_FOLDER'],'{}/'.format(archivo_voz.id), filename)
+        archivo_voz.archivo_original = path_real
+        file.save(path_real)
+        db.session.commit()
+        return archivoVozSchema.dump(archivo_voz)
+
+
+@app.route('/api/voz', methods=['POST'])
+def voz():
+    #TODO Asignar fecha creaciòn desde el servidor (maybe)
+    f_creacion = req.get('f_creacion', None)
+    email = req.get('email', None)
+    nombres = req.get('nombres',None)
+    apellidos = req.get('apellidos',None)
+    observaciones = req.get('observaciones',None)
+    archivo_id = req.get('archivo_id',None)
+    concurso_id = req.get('concurso_id',None)
+    
+    Concurso.query.get_or_404(concurso_id)
+    ArchivoVoz.query.get_or_404(archivo_id)
+
+    voz = Voz(
+        f_creacion=f_creacion,
+        email=email,
+        nombres=nombres,
+        apellidos=apellidos,
+        observaciones=observaciones,
+        archivo_id=archivo_id,
+        concurso_id=concurso_id,
+        )
+    db.session.commit()
+    vozSchema.dump(voz)
 
 
 if __name__ == '__main__':
--- api/models.py
+++ api/models.py
@@ -12,7 +12,7 @@ class UserAdmin(db.Model):
     email = db.Column(db.String(120), nullable=0, unique=1)
     contrasena = db.Column(db.String(120), nullable=0)
     #relaciones
-    concursos = db.relationship('Concurso', backref='UserAdmin', lazy=1)
+    concursos = db.relationship('Concurso', backref='user_admin', lazy=1)
 
     @property
     def rolenames(self):
@@ -20,7 +20,7 @@ class UserAdmin(db.Model):
 
     @property
     def password(self):
-        return self.constrasena
+        return self.contrasena
 
     @classmethod
     def lookup(cls, email):
@@ -51,21 +51,21 @@ usersAdminSchema = UserAdminSchema(many=1)
 class Concurso(db.Model):
     id = db.Column(db.Integer, primary_key=1)
     nombre = db.Column(db.String(120), nullable=0)
-    #TODO banner o imagen
-    #TODO url única
+    imagen_base64 = db.Column(db.Text, nullable=1)
+    url = db.Column(db.String(120), nullable=0, unique=1)
     f_inicio = db.Column(db.DateTime, nullable=0)
     f_fin = db.Column(db.DateTime, nullable=0)
     valor_paga = db.Column(db.Integer, nullable=0)
     guion = db.Column(db.String(2000), nullable=0)
     recomendaciones = db.Column(db.String(2000), nullable=0)
     #relaciones
-    user_id = db.Column(db.Integer, db.ForeignKey('UserAdmin.id'), nullable=0)
-    voces = db.relationship('Voz', backref='Concurso', lazy=1)
+    user_id = db.Column(db.Integer, db.ForeignKey('user_admin.id'), nullable=0)
+    voces = db.relationship('Voz', backref='concurso', lazy=1)
 
 
 class ConcursoSchema(ma.Schema):
     class Meta:
-        fields = ("id", "nombre", "f_inicio", "f_fin", "valor_paga", "guion", "recomendaciones", "user_id")
+        fields = ("id", "nombre", "f_inicio", "f_fin", "url", "imagen_base64", "valor_paga", "guion", "recomendaciones", "user_id")
 
 
 concursoSchema = ConcursoSchema()
@@ -79,16 +79,32 @@ class Voz(db.Model):
     email = db.Column(db.String(120), nullable=0)
     nombres = db.Column(db.String(220), nullable=0)
     apellidos = db.Column(db.String(220), nullable=0)
-    convertida = db.Column(db.Boolean, nullable=0) #estado de la voz
     observaciones = db.Column(db.String(1000))
-    #TODO archivo_original
-    #TODO archivo_convertida
-    #relaciones
-    concurso_id = db.Column(db.Integer, db.ForeignKey('Concurso.id'), nullable=0)
+    archivo_id = db.Column(db.Integer, db.ForeignKey('archivo_voz.id'), nullable=0)
+    concurso_id = db.Column(db.Integer, db.ForeignKey('concurso.id'), nullable=0)
 
 
 class VozSchema(ma.Schema):
     class Meta:
-        fields = ("id", "f_creacion", "email", "nombres", "apellidos", "convertida", "observaciones", "concurso_id")
+        fields = ("id", "f_creacion", "email", "nombres", "apellidos", "convertida", "observaciones", "concurso_id", "archivo_id")
+
+
 vozSchema = VozSchema()
 vocesSchema = VozSchema(many=1)
+
+
+class ArchivoVoz(db.Model):
+    id = db.Column(db.Integer, primary_key=1)
+    archivo_original = db.Column(db.String(120), nullable=0)
+    archivo_convertido = db.Column(db.String(120), nullable=1)
+    convertido = db.Column(db.Boolean, nullable=0, default=0)
+    voz = db.relationship('Voz', backref='archivo_voz', lazy=1)
+
+
+class ArchivoVozSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "convertido")
+
+
+archivoVozSchema = ArchivoVozSchema()
+archivosVozSchema = ArchivoVozSchema(many=1)
