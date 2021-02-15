39796e700248ea36a3012f7cee4d5c8ec20a4682
raquelandrea97 <ra.cerda@uniandes.edu.co>
2021-02-15 01:00:08 -0500

    HomeConcurso, Postulacion (parcial)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import NavBar from './components/NavBar';
 import Home from './components/Home'
 import Concursos from './components/Concursos'
+import HomeConcurso from './components/HomeConcurso'
 import { useEffect, useState } from 'react';
 
 function App() {
@@ -15,6 +16,8 @@ function App() {
 
   //onMount
   useEffect(() => {
+    //async function getConcurso
+
     const token = localStorage.getItem("access_token")
     if (token)
       setLogged(true)
@@ -36,6 +39,13 @@ function App() {
               setLogged={setLogged}
           />
           <Concursos />
+        </Route>
+        <Route path="/homeconcurso">
+          <NavBar
+            logged={logged}
+            setLogged={setLogged}
+          />
+          <HomeConcurso />
         </Route>
       </Switch>
     </Router>
---
+++ src/components/HomeConcurso.js
@@ -0,0 +1,129 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import PostulacionConcurso from './PostulacionConcurso'
+
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    icon: {
+        marginRight: theme.spacing(2),
+    },
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+        marginTop: theme.spacing(4),
+    },
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    cardMedia: {
+        paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(6),
+    },
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+    root: {
+        '& .MuiTextField-root': {
+          margin: theme.spacing(1),
+          width: '25ch',
+        },
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 180,
+        marginLeft: 12
+      },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+    button: {
+        marginTop: theme.spacing(3),
+        marginLeft: theme.spacing(1),
+        float: 'right'
+    },
+}))
+
+export default function VisualizarConcurso() {
+    const classes = useStyles();
+    const [concurso, setConcurso] = useState({})
+    const [openPostulacion, setOpenPostulacion] = useState(false)
+
+    useEffect(() => {
+        async function getConcurso(){
+            //const concurso_url = sessionStorage.getItem('concurso_url')
+            const concurso_url = 0
+            fetch('/api/url/'+concurso_url)
+            .then(resp => resp.json())
+            .then(json => {
+                console.log(json)
+                setConcurso({
+                    nombre : json.nombre,
+                    f_inicio : json.f_inicio,
+                    f_fin : json.f_fin,
+                    valor_paga : json.valor_paga,
+                    guion : json.guion,
+                    recomendaciones : json.recomendaciones
+                })
+            })
+        }
+        getConcurso()
+    })
+
+    return <div>
+        <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            className={classes.button}
+            onClick = {() => {setOpenPostulacion(true)}}
+        >
+            Postularme
+        </Button>
+
+        <Modal
+                aria-labelledby="transition-modal-login"
+                className={classes.modal}
+                open={openPostulacion}
+                onClose={() => setOpenPostulacion(false)}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                  timeout: 500,
+                }}
+            >
+                <Fade in={openPostulacion}>
+                    <div className={classes.paper}>
+                        <PostulacionConcurso 
+                            setOpen = {setOpenPostulacion}
+                        />
+                    </div>
+                </Fade>
+        </Modal>
+</div>;
+}
