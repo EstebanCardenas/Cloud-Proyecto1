c94f61e142183b2f6bcc13a9cdbc4cd1794e728d
EstebanCardenas <53832358+EstebanCardenas@users.noreply.github.com>
2021-02-16 21:33:15 -0500

    Update tasks.py

--- api/tasks.py
+++ api/tasks.py
@@ -7,8 +7,10 @@ celery = Celery('tasks', broker='redis://localhost:6379/0')
 @celery.task
 def convertir_a_mp3(archivo_id, path_origen, path_destino):
 	print(archivo_id,path_origen,path_destino)
+	print('-> Convirtiendo archivo {}'.format(archivo_id))
 	proc = subprocess.Popen(['ffmpeg', '-i', path_origen, path_destino])
 	proc.wait()
 	archivo = ArchivoVoz.query.get(int(archivo_id))
 	archivo.convertido = True
+	print('-> Archivo {} convertido'.format(archivo_id))
 	db.session.commit()
